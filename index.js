324b1a750dd5f5cfffc6e573d07f18dfb7951414
Waseem <waseemkhalo@gmail.com>
2022-11-16 23:45:05 -0500

    add images

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const app = require('express')();
 const { v4: uuidv4 } = require('uuid');
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
 
+app.use(cors());
 
+
+app.get("/",(request, response)=>{
+    response.send("please enter a path");
+});
+
+
+app.listen(process.env.PORT,()=>{ 
+    console.log(`Server listening on port ${process.env.PORT}!`);
+});
