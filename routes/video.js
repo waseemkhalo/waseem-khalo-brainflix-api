0a5edfade296b73a6cd146d234bdd98245dc773b
Waseem <waseemkhalo@gmail.com>
2022-11-17 22:22:55 -0500

    add video path

--- routes/video.js
+++ routes/video.js
@@ -6,11 +6,10 @@ const { v4: uuidv4 } = require("uuid");
 
 
 router.get("/videos", (req, res) => {
-    const videosJSON = fs.readFileSync('../data/videos.json')
+    const videosJSON = fs.readFileSync('./data/videos.json')
     const videos = JSON.parse(videosJSON);
     res.json(videos)
 
-    console.log(videos)
 })
 
 
@@ -19,11 +18,11 @@ router.get("/videos/:videoId",(req,res)=>{
 
     //req.params is an object with all URL params
     const id = req.params.videoId;
-    const videosJSON = fs.readFileSync("../data/videos.json");
+    const videosJSON = fs.readFileSync("./data/videos.json");
     const videos = JSON.parse(videosJSON);
     const video = videos.find( video=> video.id === id );
 
-    //deal w/ bad student ID - http://localhost:8080/videos/004
+    //dealing w/ invalid videoId - example http://localhost:8080/videos/004
     if(video === undefined){
         return res.status(404).send(`Video ${id} was not found`);
     }
